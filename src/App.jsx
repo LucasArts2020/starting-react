cf437b4b27cdb100313ea351366dc098f1cf7f82
Lucas Lima <lucasarts2020@gmail.com>
2025-07-14 21:24:35 -0300

    dynamic list add

--- src/App.jsx
+++ src/App.jsx
@@ -7,13 +7,24 @@ import TabButton from "./components/TabButton.jsx";
 import { useState } from "react";
 import {} from "./data.js";
 function App() {
-  const [selectedTopic, setSelectedTopic] = useState("components");
+  const [selectedTopic, setSelectedTopic] = useState();
 
   function handSelect(selectedButton) {
     setSelectedTopic(selectedButton);
     console.log(selectedTopic);
   }
-
+  let tabContent = <p>Please select a topic</p>;
+  if (selectedTopic) {
+    tabContent = (
+      <div id="tab-content">
+        <h3>{EXAMPLES[selectedTopic].title}</h3>
+        <p>{EXAMPLES[selectedTopic].description}</p>
+        <pre>
+          <code>{EXAMPLES[selectedTopic].code}</code>
+        </pre>
+      </div>
+    );
+  }
   return (
     <div>
       <Header />
@@ -21,33 +32,40 @@ function App() {
         <section id="core-concepts">
           <h2>Core Concep ts</h2>
           <ul>
-            <CoreConcept
-              title={CORE_CONCEPTS[0].title}
-              description={CORE_CONCEPTS[0].description}
-              image={CORE_CONCEPTS[0].image}
-            />
-            <CoreConcept {...CORE_CONCEPTS[1]} />
-            <CoreConcept {...CORE_CONCEPTS[2]} />
-            <CoreConcept {...CORE_CONCEPTS[3]} />
+            {CORE_CONCEPTS.map((conceptItem) => (
+              <CoreConcept key={conceptItem.title} {...conceptItem} />
+            ))}
           </ul>
         </section>
         <section id="examples">
           <h2>Examples</h2>
           <menu>
-            <TabButton onSelect={() => handSelect("components")}>
+            <TabButton
+              isSelected={selectedTopic == "components"}
+              onSelect={() => handSelect("components")}
+            >
               Componentes
             </TabButton>
-            <TabButton onSelect={() => handSelect("jsx")}>JSX</TabButton>
-            <TabButton onSelect={() => handSelect("props")}>Props</TabButton>
-            <TabButton onSelect={() => handSelect("state")}>State</TabButton>
+            <TabButton
+              isSelected={selectedTopic == "jsx"}
+              onSelect={() => handSelect("jsx")}
+            >
+              JSX
+            </TabButton>
+            <TabButton
+              isSelected={selectedTopic == "props"}
+              onSelect={() => handSelect("props")}
+            >
+              Props
+            </TabButton>
+            <TabButton
+              isSelected={selectedTopic == "state"}
+              onSelect={() => handSelect("state")}
+            >
+              State
+            </TabButton>
           </menu>
-          <div id="tab-content">
-            <h3>{EXAMPLES[selectedTopic].title}</h3>
-            <p>{EXAMPLES[selectedTopic].description}</p>
-            <pre>
-              <code>{EXAMPLES[selectedTopic].code}</code>
-            </pre>
-          </div>
+          {tabContent}
         </section>
       </main>
     </div>
